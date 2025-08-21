7b60ae7a12090acbe8e8a5fa026e6243956a47c8
Jae J4 Lo Presti <j@j4.lc>
2025-08-21 17:56:29 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# resonite-clipboard-cs
+
+This repository contains the C# bindings for the [resonite-clipboard-rs](https://github.com/Yellow-Dog-Man/resonite-clipboard-rs) library, taken from the community project [ResoniteLinuxClipboard](https://github.com/GrandtheUK/ResoniteLinuxClipboard).
+
+## Credits
+
+A few people made this possible:
+- [Grand](https://github.com/GrandtheUK)
+- [Lexi "Big Cheese"](https://github.com/LexiBigCheese)
+- [Baplar](https://github.com/Baplar)
---
+++ resonite-clipboard-cs/ResoniteClipboard.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.InteropServices;
+
+namespace resonite_clipboard_cs;
+
+public static partial class ResoniteClipboard
+{
+    private const string ClipboardLib = "libresonite_clipboard_rs";
+
+    [LibraryImport(ClipboardLib, EntryPoint = "copy_text", StringMarshalling = StringMarshalling.Utf8)]
+    public static partial void CopyText(string data);
+
+    [LibraryImport(ClipboardLib, EntryPoint = "copy_auto")]
+    public static partial void CopyAuto(byte[] data, uint data_length);
+
+    [LibraryImport(ClipboardLib, EntryPoint = "copy_with_type", StringMarshalling = StringMarshalling.Utf8)]
+    public static partial void CopyWithType(byte[] data, uint data_length, string mime_type);
+
+    [LibraryImport(ClipboardLib, EntryPoint = "available_mime_types")]
+    public static unsafe partial IntPtr AvailableMimeTypes(IntPtr sizePtr);
+
+    [LibraryImport(ClipboardLib, EntryPoint = "paste_text")]
+    public static unsafe partial IntPtr PasteText(IntPtr sizePtr);
+
+    [LibraryImport(ClipboardLib, EntryPoint = "paste_auto")]
+    public static unsafe partial IntPtr PasteAuto(IntPtr sizePtr);
+
+    [LibraryImport(ClipboardLib, EntryPoint = "paste_with_type", StringMarshalling = StringMarshalling.Utf8)]
+    public static unsafe partial IntPtr PasteWithType(string mime_type, IntPtr sizePtr);
+}
